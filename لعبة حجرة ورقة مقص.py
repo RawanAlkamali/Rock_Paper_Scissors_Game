29e351d5b9fd0635ba2ee9570468c2d3470b58cb
Rawan <143016040+RawanAlkamali@users.noreply.github.com>
2023-08-31 23:51:18 +0300

    Add files via upload

---
+++ لعبة حجرة ورقة مقص.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 16:30:31 2023
+
+@author: hicom
+"""
+print("\n\t ----------------------------------------------\n\t | Rock, Paper, Scissors | By: Rawan Alkamali |\n\t -----------------------------------------------\n")
+
+import random
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score = 0
+while True:
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Plaer:{player_score}")
+        break
